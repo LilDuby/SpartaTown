ac9f335a60370b215439fbeff37376ee9a58d730
Github <Github>
2024-05-14 10:14:27 +0900

    Add Player Move, Look
    플레이어 이동, 바라보는 방향
    타일맵 collision 설정

---
+++ Assets/Scripts/PlayerInputController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputController : TopDownController
+{
+    private Camera camara;
+
+    private void Awake()
+    {
+        camara = Camera.main;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Vector2 moveInput = value.Get<Vector2>().normalized;
+        CallMoveEvent(moveInput);
+    }
+
+    public void OnLook(InputValue value)
+    {
+        Vector2 newAim = value.Get<Vector2>();
+        Vector2 worldPos = camara.ScreenToWorldPoint(newAim);
+        newAim = (worldPos - (Vector2)transform.position).normalized;
+
+        CallLookEvent(newAim);
+    }
+}
